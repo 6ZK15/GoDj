4974757b43a63215cd7c1a4f4a5da57d07f20a9e
Eric Dowdell <ejd1321@jagmail.southalabama.edu>
2017-12-01 15:58:36 -0500

    UI components added

---
+++ GoDj/View/TextFieldView.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldView.swift
+//  GoDj
+//
+//  Created by Eric Dowdell on 12/1/17.
+//  Copyright © 2017 Omega. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldView: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
