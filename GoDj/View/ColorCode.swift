ff9e2718ac06384e7a62bdbb556df5f70cea5ef7
Mr. Horace <horacenehemiah@yahoo.com>
2017-12-02 14:28:15 -0600

    Text Field and Color Code class commit

---
+++ GoDj/View/ColorCode.swift
@@ -0,0 +1,19 @@
+//
+//  ColorCode.swift
+//  GoDj
+//
+//  Created by Nehemiah Horace on 12/2/17.
+//  Copyright © 2017 Omega. All rights reserved.
+//
+
+import UIKit
+
+class ColorCode: UIColor {
+    
+    /* purpleColor: #440e62 - RBG (68,14,91)*/
+    let purpleColor = UIColor.init(red: 0.267, green: 0.055, blue: 0.357, alpha: 1.000)
+    
+    /* blueColor: #1b1464 - RBG (27,20,100) RBG */
+    let blueColor = UIColor.init(red: 0.106, green: 0.078, blue: 0.391, alpha: 1.000)
+
+}
