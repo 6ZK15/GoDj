68fb97e66f4a4e5006ed39250df30ff070745566
Mr. Horace <horacenehemiah@yahoo.com>
2017-12-10 16:36:18 -0600

    Added View Classes: • SignUpView.swift • ForgotUsername.swift • ForgotPassword.swift

---
+++ GoDj/View/ForgotPasswordView.swift
@@ -0,0 +1,21 @@
+//
+//  ForgotPasswordView.swift
+//  GoDj
+//
+//  Created by Nehemiah Horace on 12/10/17.
+//  Copyright © 2017 Omega. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GoDj/View/SignUpView.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpView.swift
+//  GoDj
+//
+//  Created by Nehemiah Horace on 12/10/17.
+//  Copyright © 2017 Omega. All rights reserved.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
